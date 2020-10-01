d29f600ebc8b7c92b03ed00e2027634ee198fe2d
Bharat Gera <42874594+voyagerbharat@users.noreply.github.com>
2020-10-01 22:17:03 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,155 +1,58 @@
-// Copyright 2019 Souvik Biswas
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 #include <iostream>
 using namespace std;
-
-// Defining the maximum size of the queue to be 10
-#define MAX 10
-
-// Defining a class to store the member variables
-// and member functions required
-class Queue {
-  int queue[MAX], front, rear;
-
-public:
-  Queue(): front(-1), rear(-1) {}
-  void insert(int );
-  int del();
-  void display();
-};
-
-
-/*
-
-ALGORITHM FOR INSERTING AN ELEMENT IN THE QUEUE:
-
-  1) IF REAR = MAX-1
-        WRITE Overflow
-        EXIT
-
-  2) REAR = REAR + 1
-  3) QUEUE[REAR] = VALUE
-  4) EXIT
-
-*/
-
-void Queue :: insert(int value) {
-  if(rear == MAX-1) {
-    cout<<"The Queue is full !\n";
-  }
-  else {
-    rear++;
-    queue[rear] = value;
-  }
+int queue[100], n = 100, front = - 1, rear = - 1;
+void Insert() {
+   int val;
+   if (rear == n - 1)
+   cout<<"Queue Overflow"<<endl;
+   else {
+      if (front == - 1)
+      front = 0;
+      cout<<"Insert the element in queue : "<<endl;
+      cin>>val;
+      rear++;
+      queue[rear] = val;
+   }
 }
-
-
-/*
-
-ALGORITHM FOR DELETING AN ELEMENT FROM THE QUEUE:
-
-  1) IF FRONT = REAR
-        WRITE Underflow
-        RETURN -1
-        EXIT
-
-  2) FRONT = FRONT + 1
-  3) VALUE = QUEUE[FRONT]
-  4) RETURN VALUE
-  5) EXIT
-
-*/
-
-int Queue :: del() {
-  // When front is equal to rear, then the queue is empty
-  if(front == rear) {
-    cout<<"The Queue is empty !\n";
-    return -1;
-  }
-  else {
-    // Incrementing front
-    front++;
-
-    // Storing the element in front position in value
-    int value = queue[front];
-
-    return value;
-  }
+void Delete() {
+   if (front == - 1 || front > rear) {
+      cout<<"Queue Underflow ";
+      return ;
+   } else {
+      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
+      front++;;
+   }
 }
-
-// Display the elements of the Queue by incrementing
-void Queue :: display() {
-  // When front is equal to rear, then the queue is empty
-  if(front == rear) {
-    cout<<"The Queue is empty !\n";
-  }
-  else {
-    // Printing each element
-    for(int i=front+1; i<=rear; i++) {
+void Display() {
+   if (front == - 1)
+   cout<<"Queue is empty"<<endl;
+   else {
+      cout<<"Queue elements are : ";
+      for (int i = front; i <= rear; i++)
       cout<<queue[i]<<" ";
-    } cout<<endl;
-  }
+         cout<<endl;
+   }
 }
-
-
-// Main Function
 int main() {
-  int option, value;
-  Queue q;
-
-  do {
-    cout<<"\n**** MENU ****\n"
-        <<"1. Insert\n"
-        <<"2. Delete\n"
-        <<"3. Display\n"
-        <<"4. Exit\n";
-
-    cout<<"Enter your option: ";
-    cin>>option;
-
-    switch(option) {
-      case 1: cout<<"Enter the value to be inserted: ";
-              cin>>value;
-
-              q.insert(value);
-              break;
-
-      case 2: value = q.del();
-              if(value != -1) {
-                cout<<"The deleted element is: "<<value<<endl;
-              }
-              break;
-
-      case 3: q.display();
-              break;
-
-      case 4: break;
-
-      default: cout<<"Wrong option !\n";
-               break;
-    }
-  } while(option != 4);
-
-  if(option == 4) {
-    cout<< "\nTHANK YOU for using the program !\n"
-        <<"Have a good day.\n\n";
-  }
+   int ch;
+   cout<<"1) Insert element to queue"<<endl;
+   cout<<"2) Delete element from queue"<<endl;
+   cout<<"3) Display all the elements of queue"<<endl;
+   cout<<"4) Exit"<<endl;
+   do {
+      cout<<"Enter your choice : "<<endl;
+      cin<<ch;
+      switch (ch) {
+         case 1: Insert();
+         break;
+         case 2: Delete();
+         break;
+         case 3: Display();
+         break;
+         case 4: cout<<"Exit"<<endl;
+         break;
+         default: cout<<"Invalid choice"<<endl;
+      }
+   } while(ch!=4);
+   return 0;
 }
